f6eccd8dc1f9778e4395e90426d7020820f63629
unknown <siddheshjadhav2001@gmail.com>
2025-03-15 00:21:39 -0700

    this runs

--- xv6-riscv-UCR-CS202-Winter25/kernel/sysfile.c
+++ xv6-riscv-UCR-CS202-Winter25/kernel/sysfile.c
@@ -284,73 +284,84 @@ create(char *path, short type, short major, short minor)
 }
 
 uint64
-sys_symlink(void)
-{
-  //your implementation goes here
+sys_symlink(void) {
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+      return -1;
+
+  begin_op(1);
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+      end_op(1);
+      return -1;
+  }
+
+  writei(ip, 0, (uint64)(target), 0, strlen(target) + 1);
+  iunlockput(ip);
+  end_op(1);
+
   return 0;
 }
 
+
 uint64
-sys_open(void)
-{
+sys_open(void) {
   char path[MAXPATH];
+  struct inode *ip;
   int fd, omode;
   struct file *f;
-  struct inode *ip;
-  int n;
-
-  if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
-    return -1;
-
-  begin_op(ROOTDEV);
-
-  if(omode & O_CREATE){
-    ip = create(path, T_FILE, 0, 0);
-    if(ip == 0){
-      end_op(ROOTDEV);
+  if(argstr(0, path, MAXPATH) < 0 || argint(1, &omode) < 0)
       return -1;
-    }
-  } else {
-    if((ip = namei(path)) == 0){
-      end_op(ROOTDEV);
+
+  begin_op(1);
+  if((ip = namei(path)) == 0){
+      end_op(1);
       return -1;
-    }
-    ilock(ip);
-    if(ip->type == T_DIR && omode != O_RDONLY){
+  }
+  
+  ilock(ip);
+
+  int follow_count = 0;
+  while(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+      if(follow_count++ > 10) {  
+          iunlockput(ip);
+          end_op(1);
+          return -1;
+      }
+      
+      char target[MAXPATH];
+      readi(ip, 0, (uint64)(target), 0, MAXPATH);
       iunlockput(ip);
-      end_op(ROOTDEV);
+      
+      if((ip = namei(target)) == 0) {  
+          end_op(1);
+          return -1;
+      }
+      
+      ilock(ip);
+  }
+
+  if((f = filealloc()) == 0) {
+      iunlockput(ip);
+      end_op(1);
       return -1;
-    }
   }
 
-  if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
-    iunlockput(ip);
-    end_op(ROOTDEV);
-    return -1;
-  }
-
-  if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
-    if(f)
-      fileclose(f);
-    iunlockput(ip);
-    end_op(ROOTDEV);
-    return -1;
-  }
-
-  if(ip->type == T_DEVICE){
-    f->type = FD_DEVICE;
-    f->major = ip->major;
-    f->minor = ip->minor;
-  } else {
-    f->type = FD_INODE;
-  }
+  f->type = FD_INODE;
   f->ip = ip;
   f->off = 0;
-  f->readable = !(omode & O_WRONLY);
-  f->writable = (omode & O_WRONLY) || (omode & O_RDWR);
+  f->readable = 1;
+  f->writable = 1;
+
+  if((fd = fdalloc(f)) < 0) {
+      fileclose(f);
+      end_op(1);
+      return -1;
+  }
 
   iunlock(ip);
-  end_op(ROOTDEV);
+  end_op(1);
 
   return fd;
 }
